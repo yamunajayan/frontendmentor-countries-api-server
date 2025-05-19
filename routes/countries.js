bad409fdf4d1c45ef2fc3bd3480f571a4ccb23f3
yamuna <yamunajayan@gmail.com>
2025-05-18 20:19:03 -0400

    update countries api request

--- routes/countries.js
+++ routes/countries.js
@@ -3,7 +3,19 @@ import countriesData from "../data.json" assert { type: "json" };
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json(countriesData);
+  const filteredCountries = countriesData.map((country) => ({
+    name: country.name,
+    population: country.population,
+    region: country.region,
+    subregion: country.subregion,
+    capital: country.capital,
+    currencies: country.currencies,
+    languages: country.languages,
+    flags: country.flags,
+    borders: country.borders,
+    topLevelDomain: country.topLevelDomain,
+  }));
+  res.json(filteredCountries);
 });
 
 export default router;
